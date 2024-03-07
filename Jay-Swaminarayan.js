ae03403505264fe23af7543d46a81ea7a711871c
126Nabil-M <113116225+126Nabil-M@users.noreply.github.com>
2024-03-07 14:06:16 -0500

    J Word

---
+++ Jay-Swaminarayan.js
@@ -0,0 +1,29 @@
+let button;
+<body>
+
+function textswitcheroo(){
+    document.getElementById("da future").innerHTML = "I wanna be the very best, that no one ever was...."
+}
+
+function newPage(){
+    document.getElementById(id = "omrc").innerHTML = www.kateskitchenkc.com/blog/pancakes-vs-waffles-breakfast-best
+}
+
+function newImage(){
+
+    document.getElementById(id = "omrc").innerHTML =https://static1.moviewebimages.com/wordpress/wp-content/uploads/2023/07/30-all-time-best-cartoon-characters-ever-created.jpg?q=50&fit=contain&w=943&h=&dpr=1.5g
+}
+
+<button onClick="textSwitcheroo()">
+<POKEMON></POKEMON>
+</button>
+
+<button onClick="newPage()">
+Breakfast
+</button>
+
+<button onClick="newImage()">
+    Breakfast
+</button>
+
+</body>
