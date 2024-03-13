b7bfc9b47ca105d1c0502a60e72bdc34fc56bcd5
126Nabil-M <113116225+126Nabil-M@users.noreply.github.com>
2024-03-13 11:22:46 -0400

    frutas

--- Jay-Swaminarayan.js
+++ Jay-Swaminarayan.js
@@ -1,21 +1,36 @@
 let button;
+
 <body>
 
 function textswitcheroo(){
     document.getElementById("da future").innerHTML = "I wanna be the very best, that no one ever was...."
 }
-
 function newPage(){
-    document.getElementById(id = "omrc").innerHTML = www.kateskitchenkc.com/blog/pancakes-vs-waffles-breakfast-best
+    document.getElementById("omrc").innerHTML = www.kateskitchenkc.com/blog/pancakes-vs-waffles-breakfast-best
 }
-
 function newImage(){
 
-    document.getElementById(id = "omrc").innerHTML =https://static1.moviewebimages.com/wordpress/wp-content/uploads/2023/07/30-all-time-best-cartoon-characters-ever-created.jpg?q=50&fit=contain&w=943&h=&dpr=1.5g
 }
 
+let favFrutas;
+
+
+
+function localSaver(){
+    const myFrutas = {favFruit:"Pomegranate", favColor:"Red", favVitamin:"C"};
+    document.getElementById("Frutas").innerHTML = myFrutas.favFruit;
+}
+<button onClick="localSaver()">
+</button>
+
+
+
+
+
+
+
+
 <button onClick="textSwitcheroo()">
-<POKEMON></POKEMON>
 </button>
 
 <button onClick="newPage()">
