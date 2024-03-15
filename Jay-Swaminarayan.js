e44e5ce9d639f6f8cea50504cf6e81118e51c4b1
126Nabil-M <113116225+126Nabil-M@users.noreply.github.com>
2024-03-15 12:53:59 -0400

    True Final

--- Jay-Swaminarayan.js
+++ Jay-Swaminarayan.js
@@ -4,13 +4,13 @@ myFrutas.favFruit = undefined;
 <body>
 
 function textswitcheroo(){
-    document.getElementById("da future").innerHTML = "I wanna be the very best, that no one ever was...."
+    document.getElementById("da future").innerHTML = "I wanna be the very best, that no one ever was....";
 }
 function newPage(){
-    document.getElementById("omrc").innerHTML = www.kateskitchenkc.com/blog/pancakes-vs-waffles-breakfast-best
+    document.getElementById("omrc").innerHTML = www.kateskitchenkc.com/blog/pancakes-vs-waffles-breakfast-best;
 }
 function newImage(){
-    document.getElementById("Crabby").innerHTML = "images/snazzyfox.webp"
+    document.getElementById("Crabby").innerHTML = "images/snazzyfox.webp";
 
 }
 function save(){
@@ -20,8 +20,7 @@ function save(){
 
 
 function localSaver(){
-    document.getElementById("Frutas").innerHTML = myFrutas.favFruit;
-    const myFrutas = {favFruit:"Pomegranate", favColor:"Red", favVitamin:"C"};
+    document.getElementById("Frutas").innerHTML = myFrutas;
 }
 
 
